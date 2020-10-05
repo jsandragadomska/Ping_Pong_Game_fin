7c344aa5cc3b6e9953b5212a01abefc40de721df
jsandragadomska <gadomska.sandra@gmail.com>
2020-10-05 14:53:27 +0200

    paddles functions and listeners

--- pong.py
+++ pong.py
@@ -12,6 +12,7 @@ paddle_a =  turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("black")
+paddle_a.shapesize(stretch_wid = 5, stretch_len= 1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
@@ -21,6 +22,7 @@ paddle_b =  turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("black")
+paddle_b.shapesize(stretch_wid = 5, stretch_len= 1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
@@ -31,6 +33,34 @@ ball.shape("circle")
 ball.color("black")
 ball.penup()
 ball.goto(0, 0)
+
+# Move paddles function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keybord binding
+window.listen()
+window.onkeypress(paddle_a_up, "w")
+window.onkeypress(paddle_b_up, "o")
+window.onkeypress(paddle_a_down, "s")
+window.onkeypress(paddle_b_down, "l")
 
 # Main game loop
 while True:
