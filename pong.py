07ff6d960ab1db37da23ca15e50a90ab58d88495
jsandragadomska <gadomska.sandra@gmail.com>
2020-10-06 12:51:27 +0200

    ball bouncing from paddles

--- pong.py
+++ pong.py
@@ -97,3 +97,12 @@ while True:
     if ball.xcor() <  -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+    # Paddle
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
